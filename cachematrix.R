d7e4b69a23c407c6beb0e6d99d9b891cc59265d8
RaffaUrgiles <rafaelurgiles@outlook.com>
2015-07-21 15:29:49 -0500

    week 3 assignment completed and tested

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,31 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	#declare the inverse matrix with NULL by default
+    matriz <- NULL
+    
+    #set a value to the matrix
+    set <- function(y)
+    {
+        x <<-y
+        matriz <<- NULL
+    }
+    
+    #get  the current value of a matrix
+    get <- function()
+    { x }
+    
+    #function to calculate the inverse of a given matrix
+    setInverse <- function(inverse)
+    {
+        matriz <<- inverse
+    }
+    #function to return the current value of matriz
+	getInverse <- function()
+    { matriz }
+    
+    list(set=set,get=get,setInverse=setInverse,getInverse=getInverse)
+
 }
 
 
@@ -12,4 +37,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	# assign the value contained in x to the variable matriz
+    matriz <- x$getInverse()
+    
+    #check if matriz is NULL
+    if(!is.null(matriz))
+    {
+        message("data obtained from cache")
+        return(matriz)
+    }
+    data <- x$get()
+    matriz <- solve(data,...)
+    x$setInverse(matriz)
+    matriz
 }
